58e0608e5260390d178cef56d9ac37b59700e9f7
zucchi0830 <zucchi0830@gmail.com>
2022-07-18 15:28:55 +0900

    再提出:制御構文2

--- 05/if2_1.php
+++ 05/if2_1.php
@@ -4,13 +4,13 @@ $poket_money = 1000;
 $fund_raising = 101;
 
 // ここに処理を記述
-$balance = $poket_money - $fund_raising;
+// $balance = $poket_money - $fund_raising;
 
 echo 'あなたの所持金は' . $poket_money . '円です。<br>';
 
-while ($balance > 0) {
+while ($poket_money > $fund_raising) {
     echo $fund_raising . '円募金しました。<br>';
-    echo '残り残高は' . $balance . '円です。<br>';
-    $balance -= $fund_raising;
+    echo '残り残高は' . $poket_money - $fund_raising . '円です。<br>';
+    $poket_money -= $fund_raising;
 }
 echo 'あなたはこれ以上募金できません。';
--- 05/if2_5.php
+++ 05/if2_5.php
@@ -1,17 +1,30 @@
 <?php
 
-// $subjects = ['数学', '英語', '理科', '社会', '国語'];
+$subjects = ['数学', '英語', '理科', '社会', '国語'];
 
 // ここに処理を記述
-// echo1回だけ
 
-$subjects = [
-    '数学' => '明日',
-    '英語' => '明後日',
-    '理科' => '明々後日',
-    '社会' => '昨日',
-    '国語' => '中止'
-];
-foreach ($subjects as $study => $day) {
-    echo $study  . 'の試験は' . $day . 'です。<br>';
+foreach ($subjects as $study) {
+    switch ($study) {
+        case '数学':
+            $day = '明日';
+            break;
+
+        case '英語':
+            $day = '明後日';
+            break;
+
+        case '理科':
+            $day = '明々後日';
+            break;
+
+        case '社会':
+            $day = '昨日';
+            break;
+
+        default:
+            $day = '中止';
+            break;
+    }
+    echo $study .'の試験は' . $day . 'です。<br>';
 }
