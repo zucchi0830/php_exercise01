2a68abf91baf452c3bc2d8d3d8a561d7eaee743b
zucchi0830 <zucchi0830@gmail.com>
2022-07-20 21:32:24 +0900

    再々提出:制御構文1の課題(「素数1ではありません」解決git add .)

--- 04/if_5.php
+++ 04/if_5.php
@@ -1,15 +1,17 @@
 <?php
 
-$num = 1;
+$num = 50;
 
 // ここに処理を記述
 // $numが素数であれば 素数です と出力
 // $numが素数でない時は 素数ではありません と出力(1も素数ではありません)
 $flg = true;
+if ($num == 1) {
+  $flg = false;
+}
 for ($i = 2; $i < $num; $i++) {
-  if ($num % $i == 0 or $num - 1 == 0) {
+  if ($num % $i == 0) {
     $flg = false;
-    break;
   }
 }
 
