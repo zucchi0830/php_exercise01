5cc06eebec03bb4557dd648c8951d7b9aecf45c3
zucchi0830 <zucchi0830@gmail.com>
2022-07-20 21:03:42 +0900

    再々提出:制御構文2

--- 05/if2_1.php
+++ 05/if2_1.php
@@ -10,7 +10,7 @@ echo 'あなたの所持金は' . $poket_money . '円です。<br>';
 
 while ($poket_money > $fund_raising) {
     echo $fund_raising . '円募金しました。<br>';
-    echo '残り残高は' . $poket_money - $fund_raising . '円です。<br>';
     $poket_money -= $fund_raising;
+    echo '残り残高は' . $poket_money . '円です。<br>';
 }
 echo 'あなたはこれ以上募金できません。';
